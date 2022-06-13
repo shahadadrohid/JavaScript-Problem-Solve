1d55daf902f000102f5409ea95c883721643cb5b
Mohammad Shahadad Hossain <shahadadrohid@gmail.com>
2022-06-13 21:47:07 +0600

    mile to km commit

---
+++ inchtofeet.js
@@ -0,0 +1,7 @@
+function inchToFeet(inch) {
+    totalFeet = inch * 0.083;
+    return totalFeet;
+}
+
+let inchResult = inchToFeet(10);
+console.log(inchResult);
---
+++ miletokm.js
@@ -0,0 +1,7 @@
+function mileToKm(mile) {
+    totalKm = mile * 1.609;
+    return totalKm;
+}
+
+let kmResult = mileToKm(5);
+console.log(kmResult);
